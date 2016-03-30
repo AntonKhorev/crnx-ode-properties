c5471fdede3ff2af9f930ab73f2b10cefee9eb22
Anton Khorev <tony29@yandex.ru>
2016-03-30 03:05:08 +0300

    more cell layout tests

--- test/cell-layout.js
+++ test/cell-layout.js
@@ -22,10 +22,9 @@ describe("cellLayout",()=>{
 		],[ // arc layers
 			[ // arc rows
 				[[0],[1,2]],
-			],
-			[ // arc rows
+			],[
 				[[1,2],[3]],
-			],
+			]
 		])
 		assert.deepEqual(result,[[ // node layers/rows
 			[{node:'A'},{},{},{}],
@@ -34,10 +33,59 @@ describe("cellLayout",()=>{
 		],[ // arc layers
 			[ // arc rows
 				[{rt:true},{rl:true,bl:true},{bl:true},{}],
-			],
-			[ // arc rows
+			],[
 				[{},{rt:true},{rl:true,rt:true},{bl:true}],
-			],
+			]
+		]])
+	})
+	it("makes layout for N-shape",()=>{
+		const result=cellLayout([ // node layers/rows
+			['A','B'],
+			['C','D'],
+		],[ // arc layers
+			[ // arc rows
+				[[0,1],[3]],
+				[[0],[2]],
+			]
+		])
+		assert.deepEqual(result,[[ // node layers/rows
+			[{node:'A'},{node:'B'},{},{}],
+			[{},{},{node:'C'},{node:'D'}],
+		],[ // arc layers
+			[ // arc rows
+				[{bt:true,rt:true},{rl:true,rt:true},{rl:true},{bl:true}],
+				[{rt:true},{rl:true},{bl:true},{bt:true}],
+			]
+		]])
+	})
+	it("makes layout with vertical arcs going through node layers",()=>{
+		const result=cellLayout([ // node layers/rows
+			['A'],
+			['B','C'],
+			['D'],
+			['E'],
+		],[ // arc layers
+			[ // arc rows
+				[[0],[1,2]],
+			],[
+				[[2],[3]],
+			],[
+				[[0,3],[4]],
+			]
+		])
+		assert.deepEqual(result,[[ // node layers/rows
+			[{node:'A'},{},{},{},{}],
+			[{bt:true},{node:'B'},{node:'C'},{},{}],
+			[{bt:true},{},{},{node:'D'},{}],
+			[{},{},{},{},{node:'E'}],
+		],[ // arc layers
+			[ // arc rows
+				[{bt:true,rt:true},{rl:true,bl:true},{bl:true},{},{}],
+			],[
+				[{bt:true},{},{rt:true},{bl:true},{}],
+			],[
+				[{rt:true},{rl:true},{rl:true},{rl:true,rt:true},{bl:true}],
+			]
 		]])
 	})
 })
