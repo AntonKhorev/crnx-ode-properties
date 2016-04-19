f572c7efe4c549b2b879177a1a789eecbb62df7d
Anton Khorev <tony29@yandex.ru>
2016-04-19 20:08:07 +0300

    basic table

--- src/main.js
+++ src/main.js
@@ -1,14 +1,33 @@
 'use strict'
 
+const TheadLayout=require('./thead-layout')
 const data=require('./data')
 
 $(function(){
 	$('.crnx-ode-properties').each(function(){
 		const $container=$(this)
 		$container.empty()
+		const dag={}
+		const selectedNodes={}
 		for (let id in data) {
-			$container.append("<div>$$"+data[id].equation+"$$</div>")
+			dag[id]=data[id].parents
+			selectedNodes[id]=true
 		}
+		const theadLayout=new TheadLayout(dag,selectedNodes)
+		$container.append(
+			$("<table>").append(
+				$("<thead>").append(
+					$("<tr>").append(
+						theadLayout.columns.map(id=>"<th>"+data[id].name+"</th>")
+					)
+				),
+				$("<tbody>").append(
+					$("<tr>").append(
+						theadLayout.columns.map(id=>"<td>$$"+data[id].equation+"$$</td>")
+					)
+				)
+			)
+		)
 		MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 	})
 })
