3538a8f9a3f2f5b16fc94f36a2052a4b4c7df04c
Anton Khorev <tony29@yandex.ru>
2016-04-20 12:18:25 +0300

    started trying to draw arcs

--- src/main.js
+++ src/main.js
@@ -15,17 +15,27 @@ $(function(){
 		}
 		const theadLayout=new TheadLayout(dag,selectedNodes)
 		const setArcs=($cell,cell)=>{
+			/*
 			;['bt','rl','rt','bl'].forEach(dir=>{
 				if (cell[dir]) {
 					$cell.append('+'+dir)
 				}
 			})
+			*/
+			// bl
+			if (cell.bl) {
+				$cell.append(
+					"<svg width='200' height='20' viewbox='-10 -1 20 2'>"+
+					"<path d='M 0 1 A 1 1 0 0 0 -1 0 H -10' fill='none' stroke='#000' stroke-width='0.1' />"+
+					"</svg>"
+				)
+			}
 		}
 		const writeThead=()=>{
 			const $thead=$("<thead>")
 			for (let i=0;i<theadLayout.nodeLayers.length;i++) {
 				$thead.append(
-					$("<tr>").append(
+					$("<tr class='nodes'>").append(
 						theadLayout.nodeLayers[i].map(cell=>{
 							const $cell=$("<th>")
 							if (cell.node!==undefined) {
@@ -38,7 +48,7 @@ $(function(){
 				)
 				if (i<theadLayout.arcLayers.length) {
 					$thead.append(
-						theadLayout.arcLayers[i].map(row=>$("<tr>").append(
+						theadLayout.arcLayers[i].map(row=>$("<tr class='arcs'>").append(
 							row.map(cell=>{
 								const $cell=$("<th>")
 								setArcs($cell,cell)
