7f00d65e0b776ca3f64c510ffedb71bff69d8b4e
Anton Khorev <tony29@yandex.ru>
2016-04-20 19:22:22 +0300

    delete buttons

--- src/main.js
+++ src/main.js
@@ -6,60 +6,73 @@ const data=require('./data')
 $(function(){
 	$('.crnx-ode-properties').each(function(){
 		const $container=$(this)
-		$container.empty()
 		const dag={}
 		const selectedNodes={}
 		for (let id in data) {
 			dag[id]=data[id].parents
 			selectedNodes[id]=true
 		}
-		const theadLayout=new TheadLayout(dag,selectedNodes)
-		const setCellClasses=($cell,cell)=>{
-			;['node','bt','rl','rt','bl'].forEach(dir=>{
-				if (cell[dir]) {
-					$cell.addClass(dir)
-				}
-			})
-		}
-		const writeThead=()=>{
-			const $thead=$("<thead>")
-			for (let i=0;i<theadLayout.nodeLayers.length;i++) {
-				$thead.append(
-					$("<tr class='nodes'>").append(
-						theadLayout.nodeLayers[i].map(cell=>{
-							const $cell=$("<th>")
-							if (cell.node!==undefined) {
-								$cell.append(data[cell.node].name)
-							}
-							setCellClasses($cell,cell)
-							return $cell
-						})
-					)
-				)
-				if (i<theadLayout.arcLayers.length) {
+		let theadLayout=new TheadLayout(dag,selectedNodes)
+		const writeTable=()=>{
+			const setCellClasses=($cell,cell)=>{
+				;['node','bt','rl','rt','bl'].forEach(dir=>{
+					if (cell[dir]) {
+						$cell.addClass(dir)
+					}
+				})
+			}
+			const writeThead=()=>{
+				const $thead=$("<thead>")
+				for (let i=0;i<theadLayout.nodeLayers.length;i++) {
 					$thead.append(
-						theadLayout.arcLayers[i].map(row=>$("<tr class='arcs'>").append(
-							row.map(cell=>{
+						$("<tr class='nodes'>").append(
+							theadLayout.nodeLayers[i].map(cell=>{
 								const $cell=$("<th>")
+								if (cell.node!==undefined) {
+									$cell.append(data[cell.node].name)
+								}
 								setCellClasses($cell,cell)
 								return $cell
 							})
-						))
+						)
 					)
+					if (i<theadLayout.arcLayers.length) {
+						$thead.append(
+							theadLayout.arcLayers[i].map(row=>$("<tr class='arcs'>").append(
+								row.map(cell=>{
+									const $cell=$("<th>")
+									setCellClasses($cell,cell)
+									return $cell
+								})
+							))
+						)
+					}
 				}
+				return $thead
 			}
-			return $thead
-		}
-		$container.append(
-			$("<table>").append(
-				writeThead(),
-				$("<tbody>").append(
-					$("<tr>").append(
-						theadLayout.columns.map(id=>"<td>$$"+data[id].equation+"$$</td>")
+			$container.empty().append(
+				$("<table>").append(
+					writeThead(),
+					$("<tfoot>").append(
+						$("<tr>").append(
+							theadLayout.columns.map(id=>$("<td>").append(
+								$("<button type='button'>Delete</button>").click(function(){
+									delete selectedNodes[id]
+									theadLayout=new TheadLayout(dag,selectedNodes)
+									writeTable()
+								})
+							))
+						)
+					),
+					$("<tbody>").append(
+						$("<tr>").append(
+							theadLayout.columns.map(id=>"<td>$$"+data[id].equation+"$$</td>")
+						)
 					)
 				)
 			)
-		)
-		MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+			MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+		}
+		writeTable()
 	})
 })
