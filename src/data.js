03e29684b696c64170d4ddeaab526832b8468959
Anton Khorev <tony29@yandex.ru>
2016-04-25 17:17:00 +0300

    main part of integrating factor solution

--- src/data.js
+++ src/data.js
@@ -140,6 +140,18 @@ module.exports={
 				]],
 			],
 		],
+		solutions: [
+			[
+				['title',[
+					"Method of <a href='https://en.wikipedia.org/wiki/Integrating_factor'>integrating factors</a>",
+				]],
+				['main',[
+					`Rewrite the equation as \\[ ${dydt} + g(t) \\cdot y = b(t) \\]`,
+					`Introduce the integrating factor: \\[ \\mu(t) = e^{${int('g(t)','t')}} \\]`,
+					`Integrate the equation multiplied by \\( \\mu(t) \\): \\[ \\mu(t) y = ${int('\\mu(t) b(t)','t')} + C \\]`,
+				]],
+			],
+		],
 	},
 	linearHomogeneous: {
 		parents: {
