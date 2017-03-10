4edc646802610cef57d40602ef1139a9da55a1e7
Anton Khorev <tony29@yandex.ru>
2017-03-10 15:02:27 +0300

    smaller mathjax download

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,6 @@ gulpTasks(
 	gulp,
 	"ODE properties",
 	[],
-	["'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML'"], // have to quote it manually b/c contains =
+	["'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_CHTML'"], // have to quote it manually b/c contains =
 	[require.resolve('crnx-base')]
 )
