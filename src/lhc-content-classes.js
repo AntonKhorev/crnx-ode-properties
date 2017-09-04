b07fa9f8341e566c3c2d361aea4cc89ebfde2bbc
Anton Khorev <tony29@yandex.ru>
2017-09-04 14:07:16 +0300

    greek unicode chars in 2nd-order linear homogeneous constant solutions

--- src/lhc-content-classes.js
+++ src/lhc-content-classes.js
@@ -28,8 +28,8 @@ LhcContent.Base = class {
 			`general solution (with arbitrary constants \\( k_1 \\), \\( k_2 \\)):`,
 			`\\[ ${this.getRepeatedGeneralSolution(nt)} \\]`,
 			`get constants \\( k_1 \\), \\( k_2 \\) for ${ivp} solution by solving:`,
-			`\\[ ${this.getGeneralSolutionConstantsEquation(nt,1,0,'\\lambda',1)} \\]`,
-			`\\[ ${this.getGeneralSolutionConstantsSolution(nt,'',1,0,'-\\lambda',1,x0,`${y0} - \\lambda \\cdot ${x0}`)} \\]`,
+			`\\[ ${this.getGeneralSolutionConstantsEquation(nt,1,0,'λ',1)} \\]`,
+			`\\[ ${this.getGeneralSolutionConstantsSolution(nt,'',1,0,'-λ',1,x0,`${y0} - λ \\cdot ${x0}`)} \\]`,
 		]
 	}
 	getGeneralSolutionRealCase(nt) {
@@ -39,7 +39,7 @@ LhcContent.Base = class {
 			`general solution (with arbitrary constants \\( k_1 \\), \\( k_2 \\)):`,
 			`\\[ ${this.getRealGeneralSolution(nt)} \\]`,
 			`get constants \\( k_1 \\), \\( k_2 \\) for ${ivp} solution by solving:`,
-			`\\[ ${this.getGeneralSolutionConstantsEquation(nt,1,1,'\\lambda_1','\\lambda_2')} \\]`,
+			`\\[ ${this.getGeneralSolutionConstantsEquation(nt,1,1,'λ_1','λ_2')} \\]`,
 			`\\[ ${this.getGeneralSolutionConstantsSolution(nt,'\\frac1{λ_2{-}λ_1}','λ_2',-1,'-λ_1',1,`\\frac{λ_2 \\cdot ${x0} - ${y0}}{λ_2 - λ_1}`,`\\frac{λ_1 \\cdot ${x0} - ${y0}}{λ_1 - λ_2}`)} \\]`,
 		]
 	}
@@ -50,8 +50,8 @@ LhcContent.Base = class {
 			`general solution (with arbitrary constants \\( k_1 \\), \\( k_2 \\)):`,
 			`\\[ ${this.getComplexGeneralSolution(nt)} \\]`,
 			`get constants \\( k_1 \\), \\( k_2 \\) for ${ivp} solution by solving:`,
-			`\\[ ${this.getGeneralSolutionConstantsEquation(nt,1,0,'\\alpha','\\beta')} \\]`,
-			`\\[ ${this.getGeneralSolutionConstantsSolution(nt,'\\frac1{\\beta}','\\beta',0,'-\\alpha',1,x0,`\\frac{${y0} - α \\cdot ${x0}}{β}`)} \\]`,
+			`\\[ ${this.getGeneralSolutionConstantsEquation(nt,1,0,'α','β')} \\]`,
+			`\\[ ${this.getGeneralSolutionConstantsSolution(nt,'\\frac1{β}','β',0,'-α',1,x0,`\\frac{${y0} - α \\cdot ${x0}}{β}`)} \\]`,
 		]
 	}
 	getGeneralSolutionImaginaryCase(nt) {
@@ -61,60 +61,60 @@ LhcContent.Base = class {
 			`general solution (with arbitrary constants \\( k_1 \\), \\( k_2 \\)):`,
 			`\\[ ${this.getImaginaryGeneralSolution(nt)} \\]`,
 			`get constants \\( k_1 \\), \\( k_2 \\) for ${ivp} solution by solving:`,
-			`\\[ ${this.getGeneralSolutionConstantsEquation(nt,1,0,0,'\\beta')} \\]`,
+			`\\[ ${this.getGeneralSolutionConstantsEquation(nt,1,0,0,'β')} \\]`,
 			`\\[ ${this.getGeneralSolutionConstantsSolution(nt,'',1,0,0,'β^{-1}',x0,`\\frac{${y0}}{β}`)} \\]`,
 		]
 	}
 	getContentFor_generalSolutionMethod_Repeated() {
 		return nt=>[
-			`solve characteristic equation for \\( \\lambda \\):`,
+			`solve characteristic equation for \\( λ \\):`,
 			`\\[ ${this.form.characteristicEquation(nt)} \\]`,
-			`roots \\( \\lambda \\) are going to be repeated:`,
-			`\\[ \\lambda_1 = \\lambda_2 = \\lambda \\]`,
+			`roots \\( λ \\) are going to be repeated:`,
+			`\\[ λ_1 = λ_2 = λ \\]`,
 			...this.getGeneralSolutionRepeatedCase(nt),
 		]
 	}
 	getContentFor_generalSolutionMethod_Real() {
 		return nt=>[
-			`solve characteristic equation for \\( \\lambda \\):`,
+			`solve characteristic equation for \\( λ \\):`,
 			`\\[ ${this.form.characteristicEquation(nt)} \\]`,
-			`roots \\( \\lambda \\) are going to be real distinct:`,
-			`\\[ \\lambda_1 \\ne \\lambda_2 \\]`,
-			`\\[ \\lambda_1, \\lambda_2 \\in \\mathbb{R} \\]`,
+			`roots \\( λ \\) are going to be real distinct:`,
+			`\\[ λ_1 \\ne λ_2 \\]`,
+			`\\[ λ_1, λ_2 \\in \\mathbb{R} \\]`,
 			...this.getGeneralSolutionRealCase(nt),
 		]
 	}
 	getContentFor_generalSolutionMethod_Complex() {
 		return nt=>[
-			`solve characteristic equation for \\( \\lambda \\):`,
+			`solve characteristic equation for \\( λ \\):`,
 			`\\[ ${this.form.characteristicEquation(nt)} \\]`,
-			`roots \\( \\lambda \\) are going to be complex conjugate:`,
-			`\\[ \\lambda = \\alpha \\pm i \\beta \\]`,
-			`\\[ \\beta \\ne 0 \\]`,
+			`roots \\( λ \\) are going to be complex conjugate:`,
+			`\\[ λ = α \\pm i β \\]`,
+			`\\[ β \\ne 0 \\]`,
 			...this.getGeneralSolutionComplexCase(nt),
 		]
 	}
 	getContentFor_generalSolutionMethod_Imaginary() {
 		return nt=>[
-			`solve characteristic equation for \\( \\lambda \\):`,
+			`solve characteristic equation for \\( λ \\):`,
 			`\\[ ${this.form.characteristicEquation(nt)} \\]`,
-			`roots \\( \\lambda \\) are going to be complex conjugate:`,
-			`\\[ \\lambda = \\pm i \\beta = \\pm ${this.form.naturalFrequency} \\]`,
+			`roots \\( λ \\) are going to be complex conjugate:`,
+			`\\[ λ = \\pm i β = \\pm ${this.form.naturalFrequency} \\]`,
 			...this.getGeneralSolutionImaginaryCase(nt),
 		]
 	}
 	getContentFor_generalSolutionMethod() {
 		return nt=>[
-			`solve characteristic equation for \\( \\lambda \\):`,
+			`solve characteristic equation for \\( λ \\):`,
 			`\\[ ${this.form.characteristicEquation(nt)} \\]`,
-			{type:'switch',title:`roots \\( \\lambda \\) are`,content:[
-				{type:'case',title:`repeated \\( ( \\lambda_1 = \\lambda_2 = \\lambda ) \\)`,
+			{type:'switch',title:`roots \\( λ \\) are`,content:[
+				{type:'case',title:`repeated \\( ( λ_1 = λ_2 = λ ) \\)`,
 					content:this.getGeneralSolutionRepeatedCase(nt)
 				},
-				{type:'case',title:`real distinct \\( ( \\lambda_1 \\ne \\lambda_2; \\lambda_1, \\lambda_2 \\in \\mathbb{R} ) \\)`,
+				{type:'case',title:`real distinct \\( ( λ_1 \\ne λ_2; λ_1, λ_2 \\in \\mathbb{R} ) \\)`,
 					content:this.getGeneralSolutionRealCase(nt)
 				},
-				{type:'case',title:`complex conjugate pair \\( ( \\lambda = \\alpha \\pm i \\beta; \\beta \\ne 0 ) \\)`,
+				{type:'case',title:`complex conjugate pair \\( ( λ = α \\pm i β; β \\ne 0 ) \\)`,
 					content:this.getGeneralSolutionComplexCase(nt)
 				},
 			]},
@@ -133,16 +133,16 @@ LhcContent.Scalar = class extends LhcContent.Base {
 		return `${nt.x}'(0)`
 	}
 	getRepeatedGeneralSolution(nt) {
-		return `${nt.x} = k_1 e^{\\lambda t} + k_2 t e^{\\lambda t}`
+		return `${nt.x} = k_1 e^{λ t} + k_2 t e^{λ t}`
 	}
 	getRealGeneralSolution(nt) {
-		return `${nt.x} = k_1 e^{\\lambda_1 t} + k_2 e^{\\lambda_2 t}`
+		return `${nt.x} = k_1 e^{λ_1 t} + k_2 e^{λ_2 t}`
 	}
 	getComplexGeneralSolution(nt) {
-		return `${nt.x} = k_1 e^{\\alpha t} \\cos \\beta t + k_2 e^{\\alpha t} \\sin \\beta t`
+		return `${nt.x} = k_1 e^{α t} \\cos β t + k_2 e^{α t} \\sin β t`
 	}
 	getImaginaryGeneralSolution(nt) {
-		return `${nt.x} = k_1 \\cos \\beta t + k_2 \\sin \\beta t`
+		return `${nt.x} = k_1 \\cos β t + k_2 \\sin β t`
 	}
 }
 
@@ -162,28 +162,28 @@ LhcContent.ReducedSystem = class extends LhcContent.Base {
 	}
 	getRepeatedGeneralSolution(nt) {
 		return `\\left\\{ \\begin{aligned}`+
-			`${nt.x} &= k_1 e^{\\lambda t} + k_2 t e^{\\lambda t} \\\\`+
-			`${nt.y} &= (k_1 \\lambda + k_2) e^{\\lambda t} + k_2 \\lambda t e^{\\lambda t}`+
+			`${nt.x} &= k_1 e^{λ t} + k_2 t e^{λ t} \\\\`+
+			`${nt.y} &= (k_1 λ + k_2) e^{λ t} + k_2 λ t e^{λ t}`+
 		`\\end{aligned} \\right.`
 	}
 	getRealGeneralSolution(nt) {
 		return `\\left\\{ \\begin{aligned}`+
-			`${nt.x} &= k_1 e^{\\lambda_1 t} + k_2 e^{\\lambda_2 t} \\\\`+
-			`${nt.y} &= k_1 \\lambda_1 e^{\\lambda_1 t} + k_2 \\lambda_2 e^{\\lambda_2 t}`+
+			`${nt.x} &= k_1 e^{λ_1 t} + k_2 e^{λ_2 t} \\\\`+
+			`${nt.y} &= k_1 λ_1 e^{λ_1 t} + k_2 λ_2 e^{λ_2 t}`+
 		`\\end{aligned} \\right.`
 	}
 	getComplexGeneralSolution(nt) {
 		return `\\left\\{ \\begin{aligned}`+
-			`${nt.x} = \\: & k_1 e^{\\alpha t} \\cos \\beta t \\\\`+
-				`+ \\: & k_2 e^{\\alpha t} \\sin \\beta t \\\\`+
-			`${nt.y} = \\: & k_1 e^{\\alpha t} (\\alpha \\cos \\beta t - \\beta \\sin \\beta t) \\\\`+
-				`+ \\: & k_2 e^{\\alpha t} (\\alpha \\sin \\beta t + \\beta \\cos \\beta t)`+
+			`${nt.x} = \\: & k_1 e^{α t} \\cos β t \\\\`+
+				`+ \\: & k_2 e^{α t} \\sin β t \\\\`+
+			`${nt.y} = \\: & k_1 e^{α t} (α \\cos β t - β \\sin β t) \\\\`+
+				`+ \\: & k_2 e^{α t} (α \\sin β t + β \\cos β t)`+
 		`\\end{aligned} \\right.`
 	}
 	getImaginaryGeneralSolution(nt) {
 		return `\\left\\{ \\begin{aligned}`+
-			`${nt.x} &= k_1 \\cos \\beta t + k_2 \\sin \\beta t \\\\`+
-			`${nt.y} &= k_2 \\beta \\cos \\beta t - k_1 \\beta \\sin \\beta t`+
+			`${nt.x} &= k_1 \\cos β t + k_2 \\sin β t \\\\`+
+			`${nt.y} &= k_2 β \\cos β t - k_1 β \\sin β t`+
 		`\\end{aligned} \\right.`
 	}
 }
@@ -212,16 +212,16 @@ LhcContent.ReducedVector = class extends LhcContent.Base {
 	}
 	getComplexGeneralSolution(nt) {
 		return `\\begin{aligned} `+
-			`${nt.X} = \\: & k_1 e^{\\alpha t} ${nt.vec2('\\cos \\beta t','\\alpha \\cos \\beta t - \\beta \\sin \\beta t')} \\\\`+
-			        `+ \\: & k_2 e^{\\alpha t} ${nt.vec2('\\sin \\beta t','\\alpha \\sin \\beta t + \\beta \\cos \\beta t')} \\\\`+
-			        `= \\: & e^{\\alpha t} ${nt.smat2(1,0,'\\alpha','\\beta')} ${nt.smat2('\\cos \\beta t','\\sin \\beta t','- \\sin \\beta t','\\cos \\beta t')} ${nt.svec2('k_1','k_2')}`+
+			`${nt.X} = \\: & k_1 e^{α t} ${nt.vec2('\\cos β t','α \\cos β t - β \\sin β t')} \\\\`+
+			        `+ \\: & k_2 e^{α t} ${nt.vec2('\\sin β t','α \\sin β t + β \\cos β t')} \\\\`+
+			        `= \\: & e^{α t} ${nt.smat2(1,0,'α','β')} ${nt.smat2('\\cos β t','\\sin β t','- \\sin β t','\\cos β t')} ${nt.svec2('k_1','k_2')}`+
 		`\\end{aligned}`
 	}
 	getImaginaryGeneralSolution(nt) {
 		return `\\begin{aligned} `+
-			`${nt.X} = \\: & k_1 ${nt.vec2('\\cos \\beta t','-\\beta \\sin \\beta t')} \\\\`+
-			        `+ \\: & k_2 ${nt.vec2('\\sin \\beta t', '\\beta \\cos \\beta t')} \\\\`+
-			        `= \\: & ${nt.smat2(1,0,0,'\\beta')} ${nt.smat2('\\cos \\beta t','\\sin \\beta t','- \\sin \\beta t','\\cos \\beta t')} ${nt.svec2('k_1','k_2')}`+
+			`${nt.X} = \\: & k_1 ${nt.vec2('\\cos β t','-β \\sin β t')} \\\\`+
+			        `+ \\: & k_2 ${nt.vec2('\\sin β t', 'β \\cos β t')} \\\\`+
+			        `= \\: & ${nt.smat2(1,0,0,'β')} ${nt.smat2('\\cos β t','\\sin β t','- \\sin β t','\\cos β t')} ${nt.svec2('k_1','k_2')}`+
 		`\\end{aligned}`
 	}
 }
