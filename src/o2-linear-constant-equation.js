d6453cb222f089e268a3ba07e740f32f8ddba403
Anton Khorev <tony29@yandex.ru>
2017-09-08 01:06:37 +0300

    ERF particular solution - 2nd-order eqn - 'b'-coefs, sometimes ugly

--- src/o2-linear-constant-equation.js
+++ src/o2-linear-constant-equation.js
@@ -10,19 +10,20 @@ class O2LinearConstantEquation extends LinearConstantEquation {
 		const a2=this.equation.equationCoefs[2]
 		const x=this.x
 		const f=this.f
+		const expr=(...args)=>tex.sum(args)
 		const sexpr=(...args)=>tex.sum(args,o=>'{'+o+'}')
 		const cosCase=(xp,fa,fb,op)=>(
 			{type:'case',title:`\\( ${f}(t) = \\${fa} ω t \\), \\( ω ≠ 0 \\)`,content:[
 				{type:'switch',title:`coefficients satisfy`,content:[
-					{type:'case',title:`\\( `+tex.sum([a1,'≠',0])+` \\) or \\( `+tex.sum([a0,'≠',a2,'ω^2'])+` \\)`,content:[
+					{type:'case',title:`\\( `+expr(a1,'≠',0)+` \\) or \\( `+expr(a0,'≠',a2,'ω^2')+` \\)`,content:[
 						`\\[ ${xp} {=} \\frac{`+
 							sexpr('('+sexpr(a0,'-',a2,'ω^2')+')',`\\${fa} ω t`,op,a1,'ω',`\\${fb} ω t`)+
 						`}{`+
 							sexpr(a2,'^2','ω^4','+','('+sexpr(a1,'^2','-',2,a0,a2)+')','ω^2','+',a0,'^2')+
 						`} \\]`,
 					]},
-					{type:'case',title:`\\( a_1 = 0 \\) and \\( a_0 = a_2 ω^2 \\)`,content:[
-						tex.blockSum([xp,'=',op,`\\frac{t \\${fb} ω t}{2 a_2 ω}`]),
+					{type:'case',title:`\\( `+expr(a1,'=',0)+` \\) and \\( `+expr(a0,'=',a2,'ω^2')+` \\)`,content:[
+						tex.blockSum([xp,'=',op,`\\frac{t \\${fb} ω t}{`+expr(2,a2,'ω')+`}`]),
 					]},
 				]},
 			]}
@@ -30,15 +31,15 @@ class O2LinearConstantEquation extends LinearConstantEquation {
 		const expCosCase=(xp,fa,fb,op)=>(
 			{type:'case',title:`\\( ${f}(t) = e^{α t} \\${fa} ω t \\), \\( ω ≠ 0 \\)`,content:[
 				{type:'switch',title:`coefficients satisfy`,content:[
-					{type:'case',title:`\\( 2 a_2 α + a_1 ≠ 0 \\) or \\( a_0 ≠ a_2 (α^2 + ω^2) \\)`,content:[
+					{type:'case',title:`\\( `+expr(2,a2,'α','+',a1,'≠',0)+` \\) or \\( `+expr(a0,'≠',a2,'(α^2 + ω^2)')+` \\)`,content:[
 						`\\[ \\begin{aligned} `+
-							`A &= a_2 (α^2 - ω^2) + a_1 α + a_0 \\\\`+
-							`B &= (2 a_2 α + a_1) ω \\\\`+
+							`A &= `+expr(a2,'(α^2 - ω^2)','+',a1,'α','+',a0)+` \\\\`+
+							`B &= (`+expr(2,a2,'α','+',a1)+`) ω \\\\`+
 							`${xp} &= e^{α t} \\frac{A \\${fa} ω t ${op} B \\${fb} ω t}{A^2 + B^2} `+
 						`\\end{aligned} \\]`,
 					]},
-					{type:'case',title:`\\( 2 a_2 α + a_1 = 0 \\) and \\( a_0 = a_2 (α^2 + ω^2) \\)`,content:[
-						tex.blockSum([xp,'=',op,`\\frac{t e^{α t} \\${fb} ω t}{2 a_2 ω}`]),
+					{type:'case',title:`\\( `+expr(2,a2,'α','+',a1,'=',0)+` \\) and \\( `+expr(a0,'=',a2,'(α^2 + ω^2)')+` \\)`,content:[
+						tex.blockSum([xp,'=',op,`\\frac{t e^{α t} \\${fb} ω t}{`+expr(2,a2,'ω')+`}`]),
 					]},
 				]},
 			]}
@@ -51,27 +52,27 @@ class O2LinearConstantEquation extends LinearConstantEquation {
 				{type:'switch',title:`\\( ${f}(t) \\) is`,content:[
 					{type:'case',title:`\\( ${f}(t) = 1 \\)`,content:[
 						{type:'switch',title:`coefficients satisfy`,content:[
-							{type:'case',title:`\\( a_0 ≠ 0 \\)`,content:[
-								`\\[ ${xp} = \\frac1{a_0} \\]`,
+							{type:'case',title:`\\( ${a0} ≠ 0 \\)`,content:[
+								`\\[ ${xp} = \\frac1{${a0}} \\]`,
 							]},
-							{type:'case',title:`\\( a_0 = 0 \\) and \\( a_1 ≠ 0 \\)`,content:[
-								`\\[ ${xp} = \\frac{t}{a_1} \\]`,
+							{type:'case',title:`\\( ${a0} = 0 \\) and \\( ${a1} ≠ 0 \\)`,content:[
+								`\\[ ${xp} = \\frac{t}{${a1}} \\]`,
 							]},
-							{type:'case',title:`\\( a_0 = 0 \\) and \\( a_1 = 0 \\)`,content:[
-								`\\[ ${xp} = \\frac{t^2}{2 a_2} \\]`,
+							{type:'case',title:`\\( ${a0} = 0 \\) and \\( ${a1} = 0 \\)`,content:[
+								`\\[ ${xp} = \\frac{t^2}{`+expr(2,a2)+`} \\]`,
 							]},
 						]},
 					]},
 					{type:'case',title:`\\( ${f}(t) = e^{α t} \\)`,content:[
 						{type:'switch',title:`coefficients satisfy`,content:[
-							{type:'case',title:`\\( a_2 α^2 + a_1 α + a_0 ≠ 0 \\)`,content:[
-								`\\[ ${xp} = \\frac{e^{α t}}{a_2 α^2 + a_1 α + a_0} \\]`,
+							{type:'case',title:`\\( `+expr(a2,'α^2','+',a1,'α','+',a0,'≠',0)+` \\)`,content:[
+								`\\[ ${xp} = \\frac{e^{α t}}{`+expr(a2,'α^2','+',a1,'α','+',a0)+`} \\]`,
 							]},
-							{type:'case',title:`\\( a_2 α^2 + a_1 α + a_0 = 0 \\) and \\( 2 a_2 α + a_1 ≠ 0 \\)`,content:[
-								`\\[ ${xp} = \\frac{t e^{α t}}{2 a_2 α + a_1} \\]`,
+							{type:'case',title:`\\( `+expr(a2,'α^2','+',a1,'α','+',a0,'=',0)+` \\) and \\( `+expr(2,a2,'α','+',a1,'≠',0)+` \\)`,content:[
+								`\\[ ${xp} = \\frac{t e^{α t}}{`+expr(2,a2,'α','+',a1)+`} \\]`,
 							]},
-							{type:'case',title:`\\( a_2 α^2 + a_1 α + a_0 = 0 \\) and \\( 2 a_2 α + a_1 = 0 \\)`,content:[
-								`\\[ ${xp} = \\frac{t^2 e^{α t}}{2 a_2} \\]`,
+							{type:'case',title:`\\( `+expr(a2,'α^2','+',a1,'α','+',a0,'=',0)+` \\) and \\( `+expr(2,a2,'α','+',a1,'=',0)+` \\)`,content:[
+								`\\[ ${xp} = \\frac{t^2 e^{α t}}{`+expr(2,a2)+`} \\]`,
 							]},
 						]},
 					]},
