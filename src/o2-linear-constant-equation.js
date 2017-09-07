5e39c99b5bd7a23a0f8ff99e18f8421ed366c6c0
Anton Khorev <tony29@yandex.ru>
2017-09-07 23:27:39 +0300

    ERF particular solution - 2nd-order eqn - less copypaste

--- src/o2-linear-constant-equation.js
+++ src/o2-linear-constant-equation.js
@@ -1,11 +1,40 @@
 'use strict'
 
+const tex=require('./tex')
 const LinearConstantEquation=require('./linear-constant-equation')
 
 class O2LinearConstantEquation extends LinearConstantEquation {
 	erfContent() {
 		const x=this.x
 		const f=this.f
+		const cosCase=(xp,fa,fb,op)=>(
+			{type:'case',title:`\\( ${f}(t) = \\${fa} ω t \\), \\( ω ≠ 0 \\)`,content:[
+				{type:'switch',title:`coefficients satisfy`,content:[
+					{type:'case',title:`\\( a_1 ≠ 0 \\) or \\( a_0 ≠ a_2 ω^2 \\)`,content:[
+						`\\[ ${xp} {=} \\frac{(a_0 {-} a_2 ω^2) \\${fa} ω t {${op}} a_1 ω \\${fb} ω t}{a_2^2 ω^4 {+} (a_1^2 {-} 2 a_0 a_2) ω^2 {+} a_0^2} \\]`,
+					]},
+					{type:'case',title:`\\( a_1 = 0 \\) and \\( a_0 = a_2 ω^2 \\)`,content:[
+						tex.blockSum([xp,'=',op,`\\frac{t \\${fb} ω t}{2 a_2 ω}`]),
+					]},
+				]},
+			]}
+		)
+		const expCosCase=(xp,fa,fb,op)=>(
+			{type:'case',title:`\\( ${f}(t) = e^{α t} \\${fa} ω t \\), \\( ω ≠ 0 \\)`,content:[
+				{type:'switch',title:`coefficients satisfy`,content:[
+					{type:'case',title:`\\( 2 a_2 α + a_1 ≠ 0 \\) or \\( a_0 ≠ a_2 (α^2 + ω^2) \\)`,content:[
+						`\\[ \\begin{aligned} `+
+							`A &= a_2 (α^2 - ω^2) + a_1 α + a_0 \\\\`+
+							`B &= (2 a_2 α + a_1) ω \\\\`+
+							`${xp} &= e^{α t} \\frac{A \\${fa} ω t ${op} B \\${fb} ω t}{A^2 + B^2} `+
+						`\\end{aligned} \\]`,
+					]},
+					{type:'case',title:`\\( 2 a_2 α + a_1 = 0 \\) and \\( a_0 = a_2 (α^2 + ω^2) \\)`,content:[
+						tex.blockSum([xp,'=',op,`\\frac{t e^{α t} \\${fb} ω t}{2 a_2 ω}`]),
+					]},
+				]},
+			]}
+		)
 		return nt=>[
 			x._('p').firstComponentExpressionPreamble(
 				c=>`${c}a particular solution is one of the following, depending on \\( ${f}(t) \\) and the equation coefficients`
@@ -38,54 +67,10 @@ class O2LinearConstantEquation extends LinearConstantEquation {
 							]},
 						]},
 					]},
-					{type:'case',title:`\\( ${f}(t) = \\cos ω t \\), \\( ω ≠ 0 \\)`,content:[
-						{type:'switch',title:`coefficients satisfy`,content:[
-							{type:'case',title:`\\( a_1 ≠ 0 \\) or \\( a_0 ≠ a_2 ω^2 \\)`,content:[
-								`\\[ ${xp} {=} \\frac{(a_0 {-} a_2 ω^2) \\cos ω t {+} a_1 ω \\sin ω t}{a_2^2 ω^4 {+} (a_1^2 {-} 2 a_0 a_2) ω^2 {+} a_0^2} \\]`,
-							]},
-							{type:'case',title:`\\( a_1 = 0 \\) and \\( a_0 = a_2 ω^2 \\)`,content:[
-								`\\[ ${xp} = \\frac{t \\sin ω t}{2 a_2 ω} \\]`,
-							]},
-						]},
-					]},
-					{type:'case',title:`\\( ${f}(t) = \\sin ω t \\), \\( ω ≠ 0 \\)`,content:[
-						{type:'switch',title:`coefficients satisfy`,content:[
-							{type:'case',title:`\\( a_1 ≠ 0 \\) or \\( a_0 ≠ a_2 ω^2 \\)`,content:[
-								`\\[ ${xp} {=} \\frac{(a_0 {-} a_2 ω^2) \\sin ω t {-} a_1 ω \\cos ω t}{a_2^2 ω^4 {+} (a_1^2 {-} 2 a_0 a_2) ω^2 {+} a_0^2} \\]`,
-							]},
-							{type:'case',title:`\\( a_1 = 0 \\) and \\( a_0 = a_2 ω^2 \\)`,content:[
-								`\\[ ${xp} = -\\frac{t \\cos ω t}{2 a_2 ω} \\]`,
-							]},
-						]},
-					]},
-					{type:'case',title:`\\( ${f}(t) = e^{α t} \\cos ω t \\), \\( ω ≠ 0 \\)`,content:[
-						{type:'switch',title:`coefficients satisfy`,content:[
-							{type:'case',title:`\\( 2 a_2 α + a_1 ≠ 0 \\) or \\( a_0 ≠ a_2 (α^2 + ω^2) \\)`,content:[
-								`\\[ \\begin{aligned} `+
-									`A &= a_2 (α^2 - ω^2) + a_1 α + a_0 \\\\`+
-									`B &= (2 a_2 α + a_1) ω \\\\`+
-									`${xp} &= e^{α t} \\frac{A \\cos ω t + B \\sin ω t}{A^2 + B^2} `+
-								`\\end{aligned} \\]`,
-							]},
-							{type:'case',title:`\\( 2 a_2 α + a_1 = 0 \\) and \\( a_0 = a_2 (α^2 + ω^2) \\)`,content:[
-								`\\[ ${xp} = \\frac{t e^{α t} \\sin ω t}{2 a_2 ω} \\]`,
-							]},
-						]},
-					]},
-					{type:'case',title:`\\( ${f}(t) = e^{α t} \\sin ω t \\), \\( ω ≠ 0 \\)`,content:[
-						{type:'switch',title:`coefficients satisfy`,content:[
-							{type:'case',title:`\\( 2 a_2 α + a_1 ≠ 0 \\) or \\( a_0 ≠ a_2 (α^2 + ω^2) \\)`,content:[
-								`\\[ \\begin{aligned} `+
-									`A &= a_2 (α^2 - ω^2) + a_1 α + a_0 \\\\`+
-									`B &= (2 a_2 α + a_1) ω \\\\`+
-									`${xp} &= e^{α t} \\frac{A \\sin ω t - B \\cos ω t}{A^2 + B^2} `+
-								`\\end{aligned} \\]`,
-							]},
-							{type:'case',title:`\\( 2 a_2 α + a_1 = 0 \\) and \\( a_0 = a_2 (α^2 + ω^2) \\)`,content:[
-								`\\[ ${xp} = - \\frac{t e^{α t} \\cos ω t}{2 a_2 ω} \\]`,
-							]},
-						]},
-					]},
+					cosCase(xp,'cos','sin','+'),
+					cosCase(xp,'sin','cos','-'),
+					expCosCase(xp,'cos','sin','+'),
+					expCosCase(xp,'sin','cos','-'),
 				]}
 			)),
 			...x._('p').restDiffComponentExpressionContent()(nt),
